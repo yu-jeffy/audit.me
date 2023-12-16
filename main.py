a9a0cb30f675d15ee266bc0433dbe49018b388a1
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-16 01:52:26 +0000

    gpt-4-1106, test prompt, max tokens

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ load_dotenv()
 ################################################
 #  create llm
 ################################################
-llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-
+# llm = ChatOpenAI(temperature=0.3, openai_api_key=os.getenv("OPENAI_API_KEY"))
+llm = ChatOpenAI(
+    temperature=0.3, model_name="gpt-4-1106-preview"
+)
 
 ################################################
 #  vectore store
@@ -80,7 +82,7 @@ _inputs = RunnableParallel(
         chat_history=lambda x: get_buffer_string(x["chat_history"])
     )
     | CONDENSE_QUESTION_PROMPT
-    | ChatOpenAI(temperature=0.3, max_tokens=2048)
+    | ChatOpenAI(temperature=0.3, max_tokens=4096)
     | StrOutputParser(),
 )
 
@@ -105,24 +107,171 @@ print("testing prompt...")
 print(conversational_qa_chain.invoke(
     {
         "question": """Check for vulnerabilties in this smart contract:
-            contract EtherStore {
-                mapping(address => uint256) public balances;
-
-                function deposit() public payable {
-                    balances[msg.sender] += msg.value;
-                }
-
-                function withdrawFunds(uint256 _weiToWithdraw) public {
-                    require(balances[msg.sender] >= _weiToWithdraw);
-                    (bool send, ) = msg.sender.call{value: _weiToWithdraw}("");
-                    require(send, "send failed");
-
-                    // check if after send still enough to avoid underflow
-                    if (balances[msg.sender] >= _weiToWithdraw) {
-                        balances[msg.sender] -= _weiToWithdraw;
-                    }
-                }
-            }
+            import " @openzeppelin / contracts - ethereum - package /
+contracts / token / ERC20 / ERC20 .sol";
+import " @openzeppelin / contracts - ethereum - package /
+contracts / token / ERC20 / IERC20 .sol";
+import " @openzeppelin / contracts - ethereum - package /
+contracts / token / ERC20 / extensions / IERC20Metadata . sol "
+;
+import " @openzeppelin / contracts - ethereum - package /
+contracts / utils / Context .sol";
+import " @openzeppelin / contracts - ethereum - package /
+contracts / access / Ownable .sol";
+pragma solidity ^0.8.0;
+contract AirdropFaucet is Ownable {
+uint256 public dripAmount ;
+mapping ( address = > bool ) public previousRequestors ;
+address public tokenAddress ;
+uint256 public lotteryReward ;
+event TokensRequested ( address requestor ) ;
+event LotteryPlayed ( address player , bool win ) ;
+struct Proposal {
+string description ;
+uint256 yesVotes ;
+uint256 noVotes ;
+}
+mapping ( uint256 = > Proposal ) public proposals ;
+mapping ( address = > uint256 ) public depositedBalances ;
+uint256 public proposalCount ;
+constructor ( address _tokenAddress ) {
+tokenAddress = _tokenAddress ;
+uint8 decimals = ERC20 ( tokenAddress ) . decimals () ;
+dripAmount = 1000 * 10 ** decimals ;
+lotteryReward = 5000 * 10 ** decimals ;
+}
+function requestTokens ( address requestor ) external {
+require (
+previousRequestors [ requestor ] == true ,
+" Address has already requested tokens "
+) ;
+// Vulnerability : Absense of code logic or sanity
+check
+ERC20 faucetToken = ERC20 ( tokenAddress ) ;
+require (
+faucetToken . balanceOf ( address ( this ) ) >
+dripAmount ,
+" Insufficient funds in faucet "
+) ;
+// Vulnerability : Reentrancy Vulnerability
+// Vulnerability : Unhandled or mishandled
+exception
+// Transfer token to requestor
+( bool success ,) = requestor . call { value :
+dripAmount }("") ;
+previousRequestors [ requestor ] = true ;
+emit TokensRequested ( requestor ) ;
+}
+// Vulnerability : Transaction / Strategy replay
+attacks // Note : This is Included in the 2
+Vulnerabilities Set Mutation Test
+function transferWithSignature (
+address from ,
+address to ,
+uint256 amount ,
+uint256 expiration ,
+uint8 v ,
+bytes32 r ,
+bytes32 s
+) external {
+require ( block . timestamp < expiration , " Signature
+expired ") ;
+bytes32 messageHash = keccak256 ( abi . encodePacked (
+from , to , amount , expiration ) ) ;
+bytes32 prefixedHash = keccak256 ( abi . encodePacked
+("\ x19Ethereum Signed Message :\ n32",
+messageHash ) ) ;
+address signer = ecrecover ( prefixedHash , v , r , s )
+;
+require ( signer == from , " Invalid signature ") ;
+ERC20 ( tokenAddress ) . transferFrom ( from , to , amount
+) ;
+}
+// Vulnerability : Insecure Randomness
+// Vulnerability : Front - running attacks
+function playLottery ( uint256 guess ) external {
+require ( guess >= 1 && guess <= 10 , " Guess must be
+between 1 and 10") ;
+uint256 randomNumber = uint256 ( blockhash ( block .
+number - 1) ) % 10 + 1;
+if ( randomNumber == guess ) {
+ERC20 ( tokenAddress ) . transfer ( msg . sender ,
+lotteryReward ) ;
+emit LotteryPlayed ( msg . sender , true ) ;
+} else {
+emit LotteryPlayed ( msg . sender , false ) ;
+}
+}
+// Vulnerability : Inconsistent access control
+// Note : This is Included in the 2 Vulnerabilities
+Set Mutation Test
+function setLotteryReward ( uint256 _lotteryReward )
+external {
+lotteryReward = _lotteryReward ;
+}
+function withdrawTokens () external onlyOwner {
+ERC20 faucetToken = ERC20 ( tokenAddress ) ;
+uint256 amount = faucetToken . balanceOf ( address (
+this ) ) ;
+faucetToken . transfer ( msg . sender , amount ) ;
+}
+function setDripAmount ( uint256 _dripAmount ) external
+onlyOwner {
+dripAmount = _dripAmount ;
+}
+function createProposal ( string calldata description )
+external {
+proposals [ proposalCount ] = Proposal ( description ,
+0 , 0) ;
+proposalCount ++;
+}
+// Vulnerability : Governance attacks ( flash loan , etc
+.)
+function vote ( uint256 proposalId , bool support )
+external {
+ERC20 token = ERC20 ( tokenAddress ) ;
+uint256 voterBalance = token . balanceOf ( msg . sender
+) ;
+require ( proposalId < proposalCount , " Invalid
+proposal ID") ;
+if ( support ) {
+proposals [ proposalId ]. yesVotes +=
+voterBalance ;
+} else {
+proposals [ proposalId ]. noVotes += voterBalance
+;
+}
+}
+// Vulnerability : Other smart contract
+vulnerabilities ( Developer oversight )
+function depositTokens ( uint256 amount ) external {
+ERC20 token = ERC20 ( tokenAddress ) ;
+// Transfer the tokens from the user to the
+contract
+token . transferFrom ( msg . sender , address ( this ) ,
+amount ) ;
+// Update the internal accounting of the
+deposited tokens
+// depositedBalances [msg. sender ] += amount ;
+}
+function setTokenAddress ( address _tokenAddress )
+external onlyOwner {
+tokenAddress = _tokenAddress ;
+}
+// Vulnerability : Unbounded operation , including gas
+limit and call - stack depth
+function batchTransfer ( address [] calldata recipients ,
+uint256 [] calldata amounts ) external {
+require ( recipients . length == amounts . length , "
+Recipients and amounts length mismatch ") ;
+ERC20 token = ERC20 ( tokenAddress ) ;
+for ( uint256 i = 0; i < recipients . length ; i ++) {
+// Transfer tokens without any bounds or gas
+limit checks
+token . transfer ( recipients [ i ] , amounts [ i ]) ;
+}
+}
+}
             """,
         "chat_history": [],
     }
